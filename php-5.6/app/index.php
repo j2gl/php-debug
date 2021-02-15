f9a73a5a0e3e9276606c89d687f742de957faa06
Juan J. Garcia <juanjo.garcia@gmail.com>
2021-02-15 01:36:57 +0100

    add-php-5.6: PHP 5.6 working with fastcgi (fpm)

---
+++ php-5.6/app/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>PHP 5.6</title>
+    <meta content="text/html; charset=utf-8" />
+</head>  
+<body>     
+    <h1>PHP 5.6 - Development Docker Image</h1>
+    <p>
+        <?=  "PHP Version is: " . phpversion(); ?><br/>
+        <br/>
+        check <a href="info.php">phpinfo()</a>
+    </p>
+</body>
+</html>
